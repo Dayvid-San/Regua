fef2e093e9fe4d1a8e6724af7ddf56e407b3c6b1
Dayvid Santana <santana.dayvid@outlook.com>
2022-02-05 21:34:48 -0300

    add some first logic to scale

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 function sizeOfThings(){
-    var windowWidth = window.innerWidth;
-    var windowHeight = window.innerHeight;
+    let windowWidth = window.innerWidth;
+    let windowHeight = window.innerHeight;
+    let screenWidth = screen.width;
+    let screenHeight = screen.height;
     
-    var screenWidth = screen.width;
-    var screenHeight = screen.height;
     
-    document.querySelector('.window-size').innerHTML = 'A largura da janela é '+ windowWidth + ' e a altura é ' + windowHeight;
-    document.querySelector('.screen-size').innerHTML = 'A largura da tela é ' + screenWidth + ' e a altura é ' + screenHeight;
+    document.querySelector('.window-size').innerHTML = 'A largura da janela é '+ windowWidth + 'px e a altura é ' + windowHeight + 'px';
+    document.querySelector('.screen-size').innerHTML = 'A largura da tela é ' + screenWidth + 'px e a altura é ' + screenHeight + 'px';
 
-    };
+    // Ajeitar a largura em centimetros com a regua
+    const windowWidth_cm = windowWidth/37.8
+    // Função que adiciona um centímetro à regua de acordo com o espaço de tela
+};
 
+
+sizeOfThings();
+
+    // Fica ouvido as alterações no tamanho da janela
+window.addEventListener('resize', function(){
     sizeOfThings();
-
-    window.addEventListener('resize', function(){
-        sizeOfThings();
 });
